e2766beae91036d0398e023ae9b210a0f53284c6
IceRain <664073296@qq.com>
2018-09-12 15:29:43 -0400

    the very first test case

--- src/test/java/common/PokerTest.java
+++ src/test/java/common/PokerTest.java
@@ -3,7 +3,21 @@ package common;
 import junit.framework.TestCase;
 
 public class PokerTest extends TestCase {
-	public void testInit() {	
+	//The test case for reading the rank of hand.
+	public void testRank() {	
 		
+	}
+	//The method cardcount() is to count how many of each color of card is on hand.
+	public void testCardcount() {
+		PokerTest test = new PokerTest();
+		//The first test is to test the case that all card input is Spade.
+		String[] in1 = {"S2","S3","S4","S5","S6"};//The input for first test
+		int[] er1 = {5,0,0,0};//The output for first test
+		assertEquals(er1,test.cardCount(in1));
+		
+		//The second test is to test the case that most card has different color
+		String[] in2 = {"S5","H3","C4","D5","D6"};
+		int[] er2 = {1,1,2,1};
+		assertEquals(er2,test.cardCount(in2));
 	}
 }
